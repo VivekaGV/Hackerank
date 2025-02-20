5e2c6a8d341b7d13686b211bf8db593506abd280
VivekaGV <149991002+VivekaGV@users.noreply.github.com>
2025-02-20 19:21:24 +0530

    Update Sparsearray.c

--- Sparsearray.c
+++ Sparsearray.c
@@ -3,9 +3,9 @@ int* matchingStrings(int stringList_count, char** stringList, int queries_count,
     int *res=(int*)malloc(queries_count*sizeof(int*));
     
      for(int i=0;i< queries_count;i++){
-        res[i] = 0;
+        res[i]=0;
         for(int j=0;j<stringList_count;j++){
-         if (strcmp(queries[i], stringList[j]) == 0) {
+         if (strcmp(queries[i],stringList[j])==0) {
                 res[i]++;
         }  
      }
